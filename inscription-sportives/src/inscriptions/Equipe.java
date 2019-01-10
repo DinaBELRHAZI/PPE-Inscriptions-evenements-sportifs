665adf3f3ea6e99338ae98cc6dca78ba75e06be2
Dina <dina.bel1999@gmail.com>
2019-01-10 14:40:52 +0100

    TODO

---
+++ inscription-sportives/src/inscriptions/Equipe.java
@@ -0,0 +1,87 @@
+package inscriptions;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Représente une Equipe. C'est-à-dire un ensemble de personnes pouvant 
+ * s'inscrire à une compétition.
+ * 
+ */
+
+public class Equipe extends Candidat
+{
+	private static final long serialVersionUID = 4147819927233466035L;
+	private SortedSet<Personne> membres = new TreeSet<>();
+
+	
+	Equipe(Inscriptions inscriptions, String nom)
+	{
+		super(inscriptions, nom);
+	}
+
+	/**
+	 * Retourne l'ensemble des personnes formant l'équipe.
+	 */
+	
+	public SortedSet<Personne> getMembres()
+	{
+		return Collections.unmodifiableSortedSet(membres);
+	}
+	
+	/**
+	 * Ajoute une personne dans l'équipe.
+	 * @param membre
+	 * @return
+	 */
+
+	public boolean add(Personne membre)
+	{
+		membre.add(this);
+		return membres.add(membre);
+	}
+
+	/**
+	 * Supprime une personne de l'équipe. 
+	 * @param membre
+	 * @return
+	 */
+	
+	public boolean remove(Personne membre)
+	{
+		membre.remove(this);
+		return membres.remove(membre);
+	}
+	
+	
+
+	/**
+	 * Retourne les personnes que l'on peut ajouter dans cette équipe.
+	 * @return les personnes que l'on peut ajouter dans cette équipe.
+	 */
+	
+	public Set<Personne> getPersonnesAAjouter()
+	{
+		if (membres != getMembres()) {
+			return Collections.unmodifiableSet(membres);
+		}else
+			return null;
+		
+		// TODO retourner les personnes que l'on peut ajouter dans cette �quipe.
+
+	}
+	
+	@Override
+	public void delete()
+	{
+		super.delete();
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "Equipe " + super.toString();
+	}
+}
